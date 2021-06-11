409ea2c7ceaf0651ef4e342acf75c25bee86cf11
dah00 <61763829+dah00@users.noreply.github.com>
2021-06-11 13:41:03 -0700

    set up the C environment and get familiar with the basics of C programming

---
+++ C learning/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[]  = "John";
+    int characterAge = 35;
+    printf("Hello world! %s", characterName);
+    printf("\nhe was %d", characterAge) ;
+    return 0;
+}
