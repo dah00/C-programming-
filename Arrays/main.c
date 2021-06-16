11abfef3ed09c314027d4ed7e5e85b641abff1e1
dah00 <61763829+dah00@users.noreply.github.com>
2021-06-15 17:57:15 -0700

    Arrays in C programming

---
+++ Arrays/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+    int numbers[10];
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    printf("%d",numbers[3]);
+    return 0;
+}
--- C learning/main.c
+++ C learning/main.c
@@ -8,6 +8,10 @@ int main()
 //    printf("Hello world! %s", characterName);x`xx
 //    printf("\nhe was %d", characterAge) ;
 
+    char name[20];
     printf("My favorite %s is %f %s", "number", 500.0, "dollar");
+    printf("Enter your full name: ");
+    scanf("%[^\n]s", name);
+    printf("your name is %s", name);
     return 0;
 }
