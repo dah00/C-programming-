0e9166bb23d0db506b59ecfe87d5a81e1d5707cf
dah00 <61763829+dah00@users.noreply.github.com>
2021-06-15 18:33:59 -0700

    Functions in C programming

--- Functions/main.c
+++ Functions/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double cube(double num);
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("Answer: %f", cube(3));
+
+    char name[20];
+    printf("\nEnter your name: ");
+    scanf("%[^\n]s", name);
+    sayHi(name);
+    printf("\nbottom");
     return 0;
+}
+
+double cube(double num){
+    double result = num * num * num;
+    return result;
+}
+
+void sayHi(char name[]){
+    printf("Hello %s!", name);
 }
