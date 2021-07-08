32d194395ceee42cc3192c91c47fb87b13866d97
dah00 <61763829+dah00@users.noreply.github.com>
2021-07-08 15:28:08 -0700

    HackerRank exercise
    Simple exercise about how to get char, string, and sentence from the user

--- C learning/main.c
+++ C learning/main.c
@@ -8,10 +8,22 @@ int main()
 //    printf("Hello world! %s", characterName);x`xx
 //    printf("\nhe was %d", characterAge) ;
 
+/*    char ch;
+    char s[100];
+    char sen[100];
+
+    scanf("%c", &ch);
+    scanf("%s%*c", s);
+    scanf("%[^\n]s", sen);
+
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s", sen);*/
+
     char name[20];
     printf("My favorite %s is %f %s", "number", 500.0, "dollar");
     printf("Enter your full name: ");
-    scanf("%[^\n]s", name);
+    scanf("%[^\n]s%*c", name);
     printf("your name is %s", name);
     return 0;
 }
